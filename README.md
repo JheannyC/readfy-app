18ef5bba0040656b3edff41d0746cb02f1cf1b59
Jeane <jeanecardoso.qa@gmail.com>
2025-09-18 22:38:33 -0300

    Criando rotas de get, getAll e post de livros

--- README.md
+++ README.md
@@ -15,3 +15,29 @@ npm run dev
 ```
 
 Abra [http://localhost:3000](http://localhost:3000) no seu navegador para ver o resultado.
+
+
+---
+
+# API
+
+```GET: /api/books - lista todos os livros existentes```
+
+---
+
+```GET: /api/book/[id] - retorna um livro específico pelo id```
+
+--- 
+
+```POST: /api/book/register-book - cria um novo livro. O id é gerado automaticamente. Passar o body: ```
+
+```
+{
+  "titulo": "string",
+  "autor": "string",
+  "genero": "string",
+  "anoPublicacao": number,
+  "avaliacao": number
+}
+```
+--- 
---
+++ readfy-app/src/app/api/book/register/route.ts
@@ -0,0 +1,30 @@
+// app/api/books/adicionar/route.ts
+import { Book, createBook } from "@/app/types/book";
+import { promises as fs } from "fs";
+import path from "path";
+
+export async function POST(request: Request) {
+  try {
+    const body: Book = await request.json();
+    const newBook: Book = createBook(body.titulo, body.autor, body.genero, body.anoPublicacao, body.avaliacao);
+
+    const filePath = path.join(process.cwd(), "data", "books.json");
+
+    const data = await fs.readFile(filePath, "utf-8");
+    const books: Book[] = JSON.parse(data);
+
+    books.push(newBook);
+
+    await fs.writeFile(filePath, JSON.stringify(books, null, 2));
+
+    return Response.json({
+      message: "Livro adicionado com sucesso!",
+      livro: newBook,
+    });
+  } catch (error: any) {
+    return Response.json(
+      { error: "Erro ao salvar livro", details: error.message },
+      { status: 500 }
+    );
+  }
+}
